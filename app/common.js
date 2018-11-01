551e7a2b4014b93f95ce23e8df5c2f63a8fb3bd9
marcin.milewski <marcin.milewski@acaisoft.com>
2018-11-01 17:48:57 +0100

    Refactoring

--- app/common.js
+++ app/common.js
@@ -5,31 +5,33 @@ const conversationId = process.env.SLACK_CONVERSATION_ID;
 const web = new WebClient(token);
 
 var common = (function () {
+
+    const sendToSlack = function (title, message, url) {
+        console.log(`Sending a slack message: {title: '${title}', message: '${message}}'`);
+        web.chat.postMessage(
+            {
+                channel: conversationId,
+                attachments: [
+                    {
+                        "color": "#2e51a6",
+                        "title": title,
+                        "title_link": url,
+                        "text": message,
+                    }
+                ]
+            })
+            .then((res) => {
+                // `res` contains information about the posted message
+                console.log('Message sent: ', res.ts);
+            })
+            .catch(console.error);
+    };
+
     return {
         sendNotification: function (title, message, url) {
             console.log(`Sending a notification: {title: '${title}', message: '${message}}'`);
-            this.sendToSlack(title, message, url)
+            sendToSlack(title, message, url)
         },
-        sendToSlack: function (title, message, url) {
-            console.log(`Sending a slack message: {title: '${title}', message: '${message}}'`);
-            web.chat.postMessage(
-                {
-                    channel: conversationId,
-                    attachments: [
-                        {
-                            "color": "#2e51a6",
-                            "title": title,
-                            "title_link": url,
-                            "text": message,
-                        }
-                    ]
-                })
-                .then((res) => {
-                    // `res` contains information about the posted message
-                    console.log('Message sent: ', res.ts);
-                })
-                .catch(console.error);
-        }
     };
 })();
 
